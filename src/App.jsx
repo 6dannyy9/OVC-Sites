c5b78b28f0c63efe019978905f4f3c3f2a4cea19
Reta AI <reta.aisolutions@gmail.com>
2026-05-05 01:29:22 -0700

    crop 15% photo

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ const DannyCard = () => (
           width: '100%',
           height: '100%',
           objectFit: 'cover',
-          objectPosition: 'center 75%',
+          objectPosition: 'center 15%',
           display: 'block',
         }}
       />
